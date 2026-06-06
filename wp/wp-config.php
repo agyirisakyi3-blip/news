bb4304f22d20b2aeba321a7946b5c869c119c27b
agyiri sakyi <info@ezranetwork.com>
2026-06-06 09:56:08 +0000

    Fix env var detection with getenv fallback and SSL config

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -3,39 +3,47 @@
  * ServerlessWP configuration for NewsBlog
  */
 
-// ** Database settings - from environment variables ** //
-if (isset($_ENV['DB_NAME'])) {
-  define( 'DB_NAME', $_ENV['DB_NAME'] );
-}
-if (isset($_ENV['DB_USER'])) {
-  define( 'DB_USER', $_ENV['DB_USER'] );
-}
-if (isset($_ENV['DB_PASSWORD'])) {
-  define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
-}
-if (isset($_ENV['DB_HOST'])) {
-  define( 'DB_HOST', $_ENV['DB_HOST'] );
+// Helper: get env var from $_ENV or getenv()
+function env($key, $default = null) {
+    $val = getenv($key);
+    if ($val !== false && $val !== '') return $val;
+    if (isset($_ENV[$key]) && $_ENV[$key] !== '') return $_ENV[$key];
+    if (isset($_SERVER[$key]) && $_SERVER[$key] !== '') return $_SERVER[$key];
+    return $default;
 }
 
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+// ** Database settings - from environment variables ** //
+$db_name = env('DB_NAME');
+if ($db_name) define('DB_NAME', $db_name);
+
+$db_user = env('DB_USER');
+if ($db_user) define('DB_USER', $db_user);
+
+$db_pass = env('DB_PASSWORD');
+if ($db_pass) define('DB_PASSWORD', $db_pass);
+
+$db_host = env('DB_HOST');
+if ($db_host) define('DB_HOST', $db_host);
+
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication keys and salts.
  */
-define( 'AUTH_KEY',         'p0a#ViMGjUMm>Z4AaOkf.c]Ip)n2)w*35ltAaJ,6 s2UnQ+1Kl)*cAv/e|:?H2!@' );
-define( 'SECURE_AUTH_KEY',  'K~JiK=9)kKbK5zSn+-DrTdSEcr{k~Dfe8f#)2_E yJ?ebbjI/tlvGLm:Oo0Am;pc' );
-define( 'LOGGED_IN_KEY',    'Tr*U|zZq-8b,umn^!~jGrqJG[I[%+_J/59+zy3#^>_(0H+.mF[T{o]NX#2%@I;dY' );
-define( 'NONCE_KEY',        'MExNG_+NXGQI7Z%B_A+mLaTK792}|td@oK:Qt^[|@Na|}<~gomA?-mj(H{~85hfr' );
-define( 'AUTH_SALT',        '~pcI;vZ ql>y]DHZ/+;5NoL*2&KM2XL0|<]PsK_$H|jPA6+K&{8Q+lDf1WzBzU f' );
-define( 'SECURE_AUTH_SALT', 'QCpDjia+Q-ng8od|sgQ8{U.gumh`.C~zZdU<P%_{}KCE]-pU>^rstDHj|,UJSZ^+' );
-define( 'LOGGED_IN_SALT',   '|Ze3uUyC7H|A`/W^4`Sw ,hnBg(l42hJ}3;@W[#O?%ji++8;|#:K{H@<==0w=e_Z' );
-define( 'NONCE_SALT',       'E=(w7Az6?avAMG3eLk5ub-p<}CubGpYqvY-#LmwY-*}b|_<DW4G20i_VWsC{!$Aq' );
+define('AUTH_KEY', 'p0a#ViMGjUMm>Z4AaOkf.c]Ip)n2)w*35ltAaJ,6 s2UnQ+1Kl)*cAv/e|:?H2!@');
+define('SECURE_AUTH_KEY', 'K~JiK=9)kKbK5zSn+-DrTdSEcr{k~Dfe8f#)2_E yJ?ebbjI/tlvGLm:Oo0Am;pc');
+define('LOGGED_IN_KEY', 'Tr*U|zZq-8b,umn^!~jGrqJG[I[%+_J/59+zy3#^>_(0H+.mF[T{o]NX#2%@I;dY');
+define('NONCE_KEY', 'MExNG_+NXGQI7Z%B_A+mLaTK792}|td@oK:Qt^[|@Na|}<~gomA?-mj(H{~85hfr');
+define('AUTH_SALT', '~pcI;vZ ql>y]DHZ/+;5NoL*2&KM2XL0|<]PsK_$H|jPA6+K&{8Q+lDf1WzBzU f');
+define('SECURE_AUTH_SALT', 'QCpDjia+Q-ng8od|sgQ8{U.gumh`.C~zZdU<P%_{}KCE]-pU>^rstDHj|,UJSZ^+');
+define('LOGGED_IN_SALT', '|Ze3uUyC7H|A`/W^4`Sw ,hnBg(l42hJ3}@W[#O?%ji++8;|#:K{H@<==0w=e_Z');
+define('NONCE_SALT', 'E=(w7Az6?avAMG3eLk5ub-p<}CubGpYqvY-#LmwY-*}b|_<DW4G20i_VWsC{!$Aq');
 /**#@-*/
 
-$table_prefix = isset($_ENV['TABLE_PREFIX']) ? $_ENV['TABLE_PREFIX'] : 'wp_';
+$table_prefix = env('TABLE_PREFIX', 'wp_');
 
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', false);
 
 $_SERVER['HTTPS'] = 'on';
 
@@ -48,19 +56,19 @@ define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
 define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
 
 // Disable file modification (Vercel is read-only filesystem)
-define('DISALLOW_FILE_EDIT', true );
-define('DISALLOW_FILE_MODS', true );
+define('DISALLOW_FILE_EDIT', true);
+define('DISALLOW_FILE_MODS', true);
 
-// MySQL SSL
-if (!isset($_ENV['SKIP_MYSQL_SSL'])) {
-  define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL );
+// MySQL SSL (Railway requires SSL)
+if (!env('SKIP_MYSQL_SSL')) {
+  define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 }
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+  define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
